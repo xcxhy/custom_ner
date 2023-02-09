acfc5c6d5fdf14bf5bba49747272f0a8d05e0299
xcxhy <1641173099@qq.com>
2023-02-09 20:13:12 +0800

    commit some file

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 Use our code can easy to train a custom new model to infer.
 
 this project is implemented based on [spacy](https://github.com/explosion/spaCy).So I'm just a porter.
+
+First you can use the test.py to ner some car brands, types and component.
+For the model, we use a small amount of manual annotation.
+
+We simply extracted a part of car brands, types and component from the wiki, manually labeled them with a small amount, and then matched them from the text to automatically create a NER dataset. This eliminates the need to use such NER annotation tools.
+
+General annotation tools likes, 
+[Doccano](https://doccano.herokuapp.com)
+[Tagtog](https://www.tagtog.net)
+[LightTag](https://www.lighttag.io)
+[Prodigy](https://demo.prodi.gy/?=null&view_id=ner_manual)
+
+ If you have both sentences and entity words, you might as well use the get_ner_dataset.py to directly generate the files needed for training through matching.
+ **Attention:** This requires you to use the category you want to identify to name the file, for example, we use "brand.txt" to name the brand entity.
--- util.py
+++ util.py
@@ -1,6 +1,13 @@
 import os
 import json
 import pickle
+import sys
+import spacy
+import nltk
+from nltk import word_tokenize, pos_tag
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+wnl = WordNetLemmatizer() 
 
 def read_pkl(path):
     try:
@@ -46,3 +53,38 @@ def write_dict_json(path, data):
         # json.dump(dict_json, file, indent = 4)
         file.write(dict_json)
     return
+
+def get_tag(text):
+    sentence_taged = nltk.pos_tag(nltk.word_tokenize(text))
+    return sentence_taged
+
+
+def get_wordnet_pos(tag):
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('V'):
+        return wordnet.VERB
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return None
+# @profile
+def get_lemmatize(text):
+    # 获取单词的词性
+    tokens = word_tokenize(text)   # 分词
+    tagged_sent = pos_tag(tokens)  # 获取单词词性
+    
+    lemmas_sent = []
+    for tag in tagged_sent:
+    
+        wordnet_pos = get_wordnet_pos(tag[1]) or wordnet.NOUN  # tag[1]指单词词性
+        # if len(tag[0]) < 3 and tag[0].endswith('s'):
+        #     lemmas_sent.append(tag[0])
+        
+        # else:
+        lemmas_sent.append(wnl.lemmatize(tag[0], pos=wordnet_pos)) 
+    if lemmas_sent == []:
+        return 0 # tag[0]指单词本身
+    return  lemmas_sent[0]  
