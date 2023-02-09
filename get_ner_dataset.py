4ad81af698e8bde4eef90bad460fd9818eaac3b2
xcxhy <1641173099@qq.com>
2023-02-09 21:39:37 +0800

    add

--- get_ner_dataset.py
+++ get_ner_dataset.py
@@ -11,7 +11,8 @@ import numpy as np
 from collections import Counter
 from tqdm import tqdm 
 from itertools import chain
-from create_new_cate.tag_lemmat import get_lemmatize
+from util import *
+
 
 
 ex = Experiment("NER_DATA")
@@ -20,32 +21,41 @@ ex = Experiment("NER_DATA")
 class NER_Dataset(object):
     def __init__(self,config):
         self.entity_path = config['entity_path']
-        self.brand_path = config["brand_path"]
-        self.model_path = config["model_path"]
-        self.specification_path = config["specification_path"]
+
+        # self.brand_path = config["brand_path"]
+        # self.model_path = config["model_path"]
+        # self.specification_path = config["specification_path"]
         self.text_path = config["text_path"]
         self.save_dir = config["save_dir"]
         self.nums = config['nums']
+        self.batch = config['batch']
+        self.class_path = config["class_path"]
         self.text = self.read_text()
-        self.entities, self.brands, self.models, \
-            self.specifications = self.read_keywords()
+        self.classes = self.read_class()
+        self.entity_names, self.entities = self.read_keywords()
             
     def read_text(self):
         prodid_to_prodname_dict = pickle.load(open(self.text_path, "rb"))
         text = [j[0] for i,j in prodid_to_prodname_dict.items()]
         return text
-        
+    
+    def read_class(self):
+        return read_list_txt(self.class_path)
+    
     def read_keywords(self):
         result = []
-        paths = [self.entity_path, self.brand_path, self.model_path, self.specification_path]
+        for root, dirs, files in os.walk(self.entity_path):
+            file_name = files
+        
+        paths = [os.path.join(self.entity_path, name) for name in file_name]
         for path in paths:
-            with open(path, 'r') as f:
-                data = f.read().splitlines()
+            data = read_list_txt(path)
             # path_list = path.split('/')
             # file_name = os.path.splitext(path_list[-1])[0]
             # vars()[file_name] = data
             result.append(self.b_words(data))
-        return result
+        names = [name.split(".")[0] for name in file_name]
+        return names, result
     
     def b_words(self, match_words):
         for i in range(len(match_words)):
@@ -67,10 +77,11 @@ class NER_Dataset(object):
     #     return
     
     def get_dataset(self):
-        training_data = {"classes" : ["PRODUCT", "BRAND", "MODEL"], "annotations" : []}
+        methods = self.entity_names
+        training_data = {"classes" : [method.upper() for method in methods] , "annotations" : []}
         lemmatize_sentences = []
-        methods = ['entities', 'brands', 'models']
-        for sentence in tqdm(self.text[self.nums*500000:(self.nums+1)*500000]):
+        
+        for sentence in tqdm(self.text[self.nums*self.batch:(self.nums+1)*self.batch]):
             temp_dict = {}
             sentence_list = sentence.strip().split(' ')
             lemma_sentence_list = list(map(get_lemmatize, sentence_list))
@@ -81,31 +92,43 @@ class NER_Dataset(object):
             lemmatize_sentences.append(single_sentence)
             
             temp_dict['entities'] = []
-            for method in methods:
-                if method == 'entities':
-                    data = self.entities
-                    single_entity = self.match_words(single_sentence, data)
-
-                elif method == 'brands':
-                    data = self.brands
-                    single_brand = self.match_words(single_sentence, data)
-                    
-                elif method == 'models':
-                    data = self.models
-                    single_model = self.match_words(single_sentence, data)
-            if single_entity == [] and single_brand == [] and single_model == []:
+            for i in range(len(methods)):
+                data = self.entities[i]
+                single_entity = self.match_words(single_sentence, data)
+                if len(single_entity) == 0:
+                    continue
+                for value in single_entity:
+                    start, end, label = int(value[0]), int(value[1]), methods[i].upper()
+                    temp_dict['entities'].append((start, end, label))
+            if len(temp_dict['entities']) == 0:
                 continue
             else:
                 temp_dict['text'] = single_sentence
-                for value1 in single_entity:
-                    start1, end1, label1 = int(value1[0]), int(value1[1]), "PRODUCT"
-                    temp_dict['entities'].append((start1, end1, label1))
-                for value2 in single_brand:
-                    start2, end2, label2 = int(value2[0]), int(value2[1]), "BRAND"
-                    temp_dict['entities'].append((start2, end2, label2))
-                for value3 in single_model:
-                    start3, end3, label3 = int(value3[0]), int(value3[1]), "MODEL"
-                    temp_dict['entities'].append((start3, end3, label3))
+            # for method in methods:
+            #     if method == 'entities':
+            #         data = self.entities
+            #         single_entity = self.match_words(single_sentence, data)
+
+            #     elif method == 'brands':
+            #         data = self.brands
+            #         single_brand = self.match_words(single_sentence, data)
+                    
+            #     elif method == 'models':
+            #         data = self.models
+            #         single_model = self.match_words(single_sentence, data)
+            # if single_entity == [] and single_brand == [] and single_model == []:
+            #     continue
+            # else:
+            #     temp_dict['text'] = single_sentence
+            #     for value1 in single_entity:
+            #         start1, end1, label1 = int(value1[0]), int(value1[1]), "PRODUCT"
+            #         temp_dict['entities'].append((start1, end1, label1))
+            #     for value2 in single_brand:
+            #         start2, end2, label2 = int(value2[0]), int(value2[1]), "BRAND"
+            #         temp_dict['entities'].append((start2, end2, label2))
+            #     for value3 in single_model:
+            #         start3, end3, label3 = int(value3[0]), int(value3[1]), "MODEL"
+            #         temp_dict['entities'].append((start3, end3, label3))
                 
                     
                 # elif method == 'specifications':
@@ -130,13 +153,16 @@ class NER_Dataset(object):
 
 @ex.config
 def ner_data_config():
-    entity_path = "/home/data_normal/nlp/xuhao/xcxhy/Knowledge_Graph/counter_query_freq/dict/entities.txt"
-    brand_path = "/home/data_normal/nlp/xuhao/xcxhy/Knowledge_Graph/counter_query_freq/dict/brands.txt"
-    model_path = "/home/data_normal/nlp/xuhao/xcxhy/Knowledge_Graph/counter_query_freq/dict/models.txt"
-    specification_path = "/home/data_normal/nlp/xuhao/xcxhy/Knowledge_Graph/counter_query_freq/dict/specifications.txt"
-    text_path = "/home/data_normal/nlp/xuhao/xcxhy/Knowledge_Graph/triplet_items/dict_file/prodid_to_prodname_dict.pkl"
-    save_dir = "/home/data_normal/nlp/xuhao/xcxhy/Knowledge_Graph/counter_query_freq/ner_dataset"
+    entity_path = "./dataset/entity"
+    class_path = "./dataset/class.txt"
+    # entity_path = "/home/data_normal/nlp/xuhao/xcxhy/Knowledge_Graph/counter_query_freq/dict/entities.txt"
+    # brand_path = "/home/data_normal/nlp/xuhao/xcxhy/Knowledge_Graph/counter_query_freq/dict/brands.txt"
+    # model_path = "/home/data_normal/nlp/xuhao/xcxhy/Knowledge_Graph/counter_query_freq/dict/models.txt"
+    # specification_path = "/home/data_normal/nlp/xuhao/xcxhy/Knowledge_Graph/counter_query_freq/dict/specifications.txt"
+    text_path = "/Users/xcxhy/AIProject/dataset/ner/prodid_to_prodname_dict.pkl"
+    save_dir = "./dataset"
     nums = 0
+    batch = 500000
     
 _config = ner_data_config()
 
